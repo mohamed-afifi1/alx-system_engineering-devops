2ab16776dbacf281c7394879b60004c7e4fc0c9e
root <root@DESKTOP-HU468P5.localdomain>
2024-07-29 22:05:20 +0300

    todo

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Using https://jsonplaceholder.typicode.com
+"""
 import json
 import requests
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get("{}/users".format(url,)).json()
 
-    with open("todo_all_employees.json", "w") as json_file:
-        for employee in users:
-            todos = requests.get("{}/todos?userId={}".format(
-                                 url, employee.get("id"))).json()
-            json.dump({employee.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": employee.get("username")
-            } for t in todos]}, json_file)
+url = "https://jsonplaceholder.typicode.com"
+
+if __name__ == '__main__':
+    data = {}
+    users_res = requests.get('{}/users'.format(url)).json()
+    todos_res = requests.get('{}/todos'.format(url)).json()
+    for user in users_res:
+        user_name = user.get('username')
+        id = user.get('id')
+        todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+        user_data = list(map(
+            lambda x: {
+                'username': user_name,
+                'task': x.get('title'),
+                'completed': x.get('completed')
+            },
+            todos
+        ))
+        data['{}'.format(id)] = user_data
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(data, json_file)
