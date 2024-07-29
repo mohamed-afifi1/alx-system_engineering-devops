ee0b116d9446f199f116db40c7b3b00e92a53c57
root <root@DESKTOP-HU468P5.localdomain>
2024-07-29 22:00:39 +0300

    test

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,14 +4,15 @@ import json
 import requests
 
 if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("{}/users".format(url,)).json()
+
     with open("todo_all_employees.json", "w") as json_file:
-        for userid in range(1, 11):
-            url = "https://jsonplaceholder.typicode.com/"
-            employee = requests.get("{}/users/{}".format(url, userid)).json()
-            todos = requests.get("{}/todos?userId={}".format(url, userid))\
-                .json()
-            json.dump({userid: [{
-                    "task": t.get("title"),
-                    "completed": t.get("completed"),
-                    "username": employee.get("username")
-                } for t in todos]}, json_file)
+        for employee in users:
+            todos = requests.get("{}/todos?userId={}".format(
+                                 url, employee.get("id"))).json()
+            json.dump({employee.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": employee.get("username")
+            } for t in todos]}, json_file)
