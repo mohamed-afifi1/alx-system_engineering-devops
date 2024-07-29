0b5c8ed0251a36a34829fb9f7dd6e2c6dc7c5fb2
root <root@DESKTOP-HU468P5.localdomain>
2024-07-29 20:34:06 +0300

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == '__main__':
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(f"{url}/users/{id}").json()
+    todos = requests.get(f"{url}/todos?userId={id}").json()
+    complete = []
+    for task in todos:
+        if task.get("completed") is True:
+            complete.append(task.get("title"))
+    print(f"Employee {employee.get('name')} is done with tasks({len(complete)}/{len(todos)}):")
+    for task in complete:
+        print(f"     {task}")
+
+
