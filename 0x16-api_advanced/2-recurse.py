231c7ec350bb54c7cc4f74449e90f929424fe073
root <root@DESKTOP-HU468P5.localdomain>
2024-08-13 19:44:23 +0300

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,15 +5,16 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=''):
     """recursive hot posts of reddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         after = data['data']['after']
         for post in data['data']['children']:
             hot_list.append(post['data']['title'])
-
-    if after is not None:
-        recurse(subreddit, hot_list, after)
-
-    return hot_list
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    else:
+        return None
