9b61e5f35e87546c2a42ceb43ac4320dbe515bf3
root <root@DESKTOP-HU468P5.localdomain>
2024-08-22 08:53:43 +0300

    test

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # fix_holperton_user_hard
 
-exec { "fix_holperton_user_hard":
-  command => "sed -i '/^holberton/s/5/5000/' /etc/security/limits.conf",
+exec { 'fix_holperton_user_hard':
+  command => 'sed -i "/^holberton/s/5/5000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 # fix_holperton_user_soft
-exec { "fix_holperton_user_soft":
-  command => "sed -i '/^holberton/s/4/5000/' /etc/security/limits.conf",
+exec { 'fix_holperton_user_soft':
+  command => 'sed -i "/^holberton/s/4/5000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
